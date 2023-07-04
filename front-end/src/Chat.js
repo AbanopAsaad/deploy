e6bb425211d51b1d56bb2e408410326423ba139f
AbanopAsaad <57320523+AbanopAsaad@users.noreply.github.com>
2023-07-04 04:40:46 +0300

    Update Chat.js

--- front-end/src/Chat.js
+++ front-end/src/Chat.js
@@ -24,7 +24,7 @@ function Chat() {
     }
 
     // Connect to the WebSocket server with the username as a query parameter
-    const newSocket = new WebSocket("ws://127.0.0.1:8000/ws/chat/");
+    const newSocket = new WebSocket("wss://bomba3.pythonanywhere.com/ws/chat/");
     setSocket(newSocket);
 
     newSocket.onopen = () => console.log("WebSocket connected");
@@ -63,7 +63,7 @@ function Chat() {
 
   const fetchChatHistory = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:8000/api/messages/");
+       const response = await axios.get("https://bomba3.pythonanywhere.com/api/messages/");
       setMessages(
         response.data.map((message) => ({
           content: message.content,
